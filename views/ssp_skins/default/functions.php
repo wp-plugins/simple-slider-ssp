1a295b3fdaa1b6f8f087f1f4cb98f3cd7fbac639
Muneeb <Muneeb@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-07 13:18:38 +0000

    Version 0.2
    git-svn-id: https://plugins.svn.wordpress.org/simple-slider-ssp/tags/0.2@635344 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ views/ssp_skins/default/functions.php
@@ -0,0 +1,34 @@
+<?php
+
+function muneeb_ssp_default_slider_skin_enqueue() {
+
+	if ( is_admin() ) return FALSE;
+
+	$flex_stylesheet = plugins_url( 'lib/flexslider.css', __FILE__ );
+	$flex_script = plugins_url( 'lib/jquery.flexslider-min.js', __FILE__ );
+
+	wp_enqueue_style( 'flexslider-css', $flex_stylesheet );
+	wp_enqueue_script( 'jquery' );
+	wp_enqueue_script( 'flexslider', $flex_script );
+
+}
+
+function muneeb_ssp_default_slider_skin_wp_head() {
+
+	
+	//stylesheet
+	echo '<style>';
+		include 'lib/style.css';
+	echo '</style>';
+	//stylesheet end
+
+}
+
+function muneeb_ssp_default_slider_skin_hooks() {
+
+	add_action( 'init', 'muneeb_ssp_default_slider_skin_enqueue' );
+	add_action( 'wp_head', 'muneeb_ssp_default_slider_skin_wp_head', 9 );
+
+}
+
+?>
